52a9cd436701bb06d12e1bb4773715355af46f3a
ivan lashin <iw.lasgin2011@gmail.com>
2021-04-24 17:23:41 +0300

    add auto movement

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -5,16 +5,39 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] private Transform _center;
     [SerializeField] private Transform _targetForY;
 
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _manualSpeed = 10f;
+    
+    [SerializeField] private float _autoRotationSpeed = 2f;
 
-    private void Update()
+    [SerializeField] private float _startAutoFollowTime = 2f;
+
+    private float _currentIdleTime = 0f;
+
+    private void UpdateRotation(float delta)
     {
-        UpdateRotation();
+        if (TryUpdateManualRotation())
+        {
+            _currentIdleTime = 0;
+            return;
+        }
+
+        if (IsAutoControl())
+        {
+            UpdateAutoRotation(delta);
+        }
+        
+        _currentIdleTime += delta;
+    }
+
+    private bool IsAutoControl()
+    {
+        return _currentIdleTime > _startAutoFollowTime;
     }
 
     private void LateUpdate()
     {
         UpdateCameraPosition();
+        UpdateRotation(Time.deltaTime);
     }
 
     private void UpdateCameraPosition()
@@ -22,12 +45,27 @@ public class FollowCamera : MonoBehaviour
         transform.position = GetTargetPosition();
     }
 
-    private void UpdateRotation()
+    private bool TryUpdateManualRotation()
     {
-        if (Input.GetMouseButton(0))
+        if (!Input.GetMouseButton(0))
         {
-            transform.eulerAngles += _speed * new Vector3(0f, Input.GetAxis("Mouse X"), 0f);
+            return false;
         }
+        
+        transform.eulerAngles += _manualSpeed * new Vector3(0f, Input.GetAxis("Mouse X"), 0f);
+
+        return true;
+    }
+
+    private void UpdateAutoRotation(float deltaTime)
+    {
+        var targetNoY = new Vector3(_targetForY.position.x, 0f, _targetForY.position.z);
+        var centerNoY = new Vector3(_center.position.x, 0f, _center.position.z);
+        var needDirection = (targetNoY - centerNoY).normalized;
+        var needAngle = Mathf.Atan2(needDirection.x, needDirection.z) * Mathf.Rad2Deg;
+        Quaternion needRotation = Quaternion.Euler(new Vector3(0, needAngle, 0));
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, needRotation, _autoRotationSpeed * deltaTime);
     }
     
     private Vector3 GetTargetPosition()
