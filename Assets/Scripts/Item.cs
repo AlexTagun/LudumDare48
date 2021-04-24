585dc875bba88ef05db61d120022a443dd8694c6
alextagun <alextagun@gmail.com>
2021-04-24 17:02:05 +0300

    add drag and drop

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IItem {
+    Sprite Icon { get; }
+}
+
+public class Item : MonoBehaviour, IItem {
+    public Sprite Icon { get; }
+}
+
+public class Shield : IItem {
+    public Sprite Icon { get; }
+}
