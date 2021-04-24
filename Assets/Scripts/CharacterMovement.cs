f6ce8ca8309416d8aef841b9ef2ec1ad12c55419
ivan lashin <iw.lasgin2011@gmail.com>
2021-04-24 21:42:45 +0300

    add character movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody _body;
+    
+    [SerializeField] private float Speed = 5f;
+    
+    [SerializeField] private Vector3 _input = Vector3.zero;
+    
+    public void SetInput(Vector3 input)
+    {
+        _input = input;
+    }
+
+    private void FixedUpdate()
+    {
+        _body.transform.forward = _input;
+        _body.MovePosition(_body.position + _input * Speed * Time.fixedDeltaTime);
+    }
+}
