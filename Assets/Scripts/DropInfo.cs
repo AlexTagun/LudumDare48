3025be64b60e9c6e0a3bdca0089809b8487b7004
ivan lashin <iw.lasgin2011@gmail.com>
2021-04-25 10:33:42 +0300

    add drop info

---
+++ Assets/Scripts/DropInfo.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class DropInfo : MonoBehaviour
+{
+    public int Weight;
+}
--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<DropInfo> _dropInfos;
+    
+    public DropInfo GetDropInfo()
     {
-        
-    }
+        var allWeight = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach (var dropInfo in _dropInfos)
+        {
+            allWeight += dropInfo.Weight;
+        }
+
+        var randWeight = Random.Range(1, allWeight + 1);
+
+        var controlWeight = 0;
+
+        foreach (var dropInfo in _dropInfos)
+        {
+            var leftEdge = controlWeight;
+            controlWeight += dropInfo.Weight;
+            var rightEdge = controlWeight;
+
+            if (leftEdge < randWeight && randWeight <= rightEdge)
+            {
+                return dropInfo;
+            }
+        }
+            
+        return null;
     }
 }
