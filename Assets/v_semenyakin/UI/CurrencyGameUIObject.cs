1b834f5a9f585fe4988616776fec34131f7437bd
vsemenyakin <semenyakinVS@gmail.com>
2021-04-25 22:04:29 +0300

    - Connected Game Currencies UI to Wallet to show actual amount of gold

---
+++ Assets/v_semenyakin/UI/CurrencyGameUIObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CurrencyGameUIObject : MonoBehaviour
+{
+    private void FixedUpdate() {
+        updateCurrencyAmountsText();
+    }
+
+    private void updateCurrencyAmountsText() {
+        int actualGoldAmount = _wallet.getCurrency(CurrencyTypes.ECurrencyType.Gold);
+        
+        if (actualGoldAmount != _showingGoldAmount) {
+            _goldAmountText.text = actualGoldAmount.ToString();
+            _showingGoldAmount = actualGoldAmount;
+        }
+    }
+
+    [SerializeField] private Wallet _wallet = null;
+
+    private int _showingGoldAmount = 0;
+    [SerializeField] private TMPro.TextMeshProUGUI _goldAmountText = null;
+}
