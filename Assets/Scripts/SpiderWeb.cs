ebfd93c8734555f2417b667c4183a98ddfa465da
alextagun <alextagun@gmail.com>
2021-04-25 17:24:47 +0300

    add spider web

---
+++ Assets/Scripts/SpiderWeb.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderWeb : MonoBehaviour {
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private Collider сollider;
+    [SerializeField] private float damage;
+    [SerializeField] private GameObject fireEffect;
+    
+
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("Hero")) return;
+
+        var hero = other.GetComponent<Hero>();
+
+        if (!(hero.GetItem() is Torch || !hero.CanDoAction())) {
+            var health = other.GetComponent<Health>();
+            health.Damage(damage);
+            return;
+        }
+
+        hero.SpendActionPoint();
+        сollider.enabled = false;
+        Die();
+    }
+
+    private void Die() {
+        fireEffect.SetActive(true);
+        rigidbody.isKinematic = false;
+        rigidbody.AddForce(-Camera.main.transform.forward * 10, ForceMode.Impulse);
+        rigidbody.AddTorque(new Vector3(200, 200, 200), ForceMode.Impulse);
+
+        StartCoroutine(DestroyCoroutine());
+    }
+
+    private IEnumerator DestroyCoroutine() {
+        yield return new WaitForSeconds(4);
+        Destroy(gameObject);
+    }
+}
